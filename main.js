ae77af0b34904c7fbf9fbd078bfc3be052f43600
M_abedalnabi <abedalnabi96@gmail.com>
2021-05-14 01:47:37 +0300

    pulse check

--- main.js
+++ main.js
@@ -12,3 +12,45 @@ app.get("/users", (req, res, next) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+//pulse check 
+//1
+const logUsers = (req, res, next)=>{
+  console.log(users)
+  next()
+}
+//2
+app.use(logUsers)
+
+//3
+const logMethod = (req, res, next)=>{
+  console.log(req.method)
+  next()
+}
+// app.use("/users", logMethod, (req, res, next)=>{
+
+// });
+//4
+app.use(express.json())
+//5
+app.use("/users",logMethod,(req, res, next)=>{
+  if (users.length === 0) {
+      const err = new Error("No users")
+      err.status = 500
+      next(err)
+      }
+      res.json(users)
+})
+
+app.use((err, req, res, next)=>{
+  res.status(err.status);
+  res.json({
+    error: {
+      status: err.status,
+      message: err.message,
+    },
+  })
+})
+
+
